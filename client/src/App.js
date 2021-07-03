e5403f8d65940f6f0e861c6be3bd2f5778e8213a
Tirtharaj <tirtharajjana615@gmail.com>
2021-07-03 16:00:57 +0530

    upload and display ,message

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
+import FileUpload from './components/FileUpload';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div className="container mt-4">
+      <h4 className="display-4 mb-4 text-center">
+        <i className="fab fa-react"></i>React File Upload
+      </h4>
+
+      <FileUpload />
     </div>
   );
 }
